074737e350934738e3da0bded7b6ecbe789b42bb
Peter Kok <kretep@users.noreply.github.com>
2020-12-20 20:32:23 +0100

    Refactor, put drawing in separate module

--- nsdash.py
+++ nsdash.py
@@ -1,64 +1,42 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
 
-import os
 import time
 from datetime import datetime
 
 import logging
 from lib.waveshare_epd import epd2in13_V2
-from PIL import Image, ImageDraw, ImageFont
 
 from nsdata import NSData
-
+from nsdraw import NSDraw
 
 logging.basicConfig(level=logging.DEBUG)
-
-def get_time():
-    # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
-    return datetime.now().strftime("%H:%M")
 
 try:
     logging.info("epd2in13_V2 Demo")
 
-    nsdata = NSData()
-
     height = 122 #epd.width?
     width = 250 #epd.height?
-    black = 0
-    white = 1
-    image = Image.new('1', (width, height), white)
-    draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype(os.environ['NSDASH_FONT'], size=42)
-    font2 = ImageFont.truetype(os.environ['NSDASH_FONT'], size=20)
+    nsdata = NSData()
+    nsdraw = NSDraw(width, height)
 
     epd = epd2in13_V2.EPD()
     logging.info("init and Clear")
     epd.init(epd.FULL_UPDATE)
     #epd.Clear(0xFF)
-    epd.displayPartBaseImage(epd.getbuffer(image))
+    epd.displayPartBaseImage(epd.getbuffer(nsdraw.image))
     epd.init(epd.PART_UPDATE)
 
     while True:
         # Retrieve data
         data = nsdata.get_data()
-        fvalue = f'{data["sgv"]}{data["direction"]}'
-        #fvalue = '%.1f%s' % (value, direction)
 
-        # Draw on image
-        draw.rectangle((0, 0, width, height), fill=white)
-        draw.text((20, 20), fvalue, font = font, fill = black)
-        size = draw.textsize(fvalue, font)
-        draw.text((20 + size[0], 20), f'{data["delta"]}\n[{data["minutes_ago"]}m]', font=font2, fill=black)
-        
-        time_text = get_time()
-        time_dim = draw.textsize(time_text, font2)
-        draw.text((width - time_dim[0], 0), get_time(), font=font2)
+        # Draw
+        nsdraw.draw_data(data)
 
-        # Render image
-        print(time_text, fvalue)
-        epd.displayPartial(epd.getbuffer(image))
+        # Display
+        epd.displayPartial(epd.getbuffer(nsdraw.image))
 
         # Sleep until the next minute
         seconds_to_go = 60 - datetime.now().second
---
+++ nsdraw.py
@@ -0,0 +1,37 @@
+from PIL import Image, ImageDraw, ImageFont
+from datetime import datetime
+import os
+
+black = 0
+white = 1
+
+class NSDraw:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.image = Image.new('1', (width, height), white)
+        self.draw = ImageDraw.Draw(self.image)
+
+        self.font = ImageFont.truetype(os.environ['NSDASH_FONT'], size=42)
+        self.font2 = ImageFont.truetype(os.environ['NSDASH_FONT'], size=20)
+
+    def draw_data(self, data):
+        draw = self.draw
+
+        # Clear
+        draw.rectangle((0, 0, self.width, self.height), fill=white)
+        
+        # Main value
+        main_text = f'{data["sgv"]}{data["direction"]}'
+        draw.text((20, 20), main_text, font = self.font, fill = black)
+
+        # Delta
+        main_size = draw.textsize(main_text, self.font)
+        delta_text = f'{data["delta"]}\n[{data["minutes_ago"]}m]'
+        draw.text((20 + main_size[0], 20), delta_text, font=self.font2, fill=black)
+        
+        # Draw time
+        time_text = datetime.now().strftime("%H:%M")
+        time_dim = draw.textsize(time_text, self.font2)
+        draw.text((self.width - time_dim[0], 0), time_text, font=self.font2)
