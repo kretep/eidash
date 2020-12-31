785e31b64003f0ff7421c9d890c94e4388b33e89
Peter Kok <kretep@users.noreply.github.com>
2020-12-31 09:28:01 +0100

    Improve moon phase accuracy
    because it's so wobbly!

--- data/ephem.py
+++ data/ephem.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import ephem
 
 class EphemData:
@@ -8,12 +8,23 @@ class EphemData:
 
     def get_data(self):
         return {
-            'moon_age_fraction': self.get_moon_age_fraction()
+            'moon_age_fraction': self.get_moon_age_fraction(datetime.now())
         }
 
-    def get_moon_age_fraction(self):
-        date = ephem.Date(datetime.datetime.now())
-        nnm = ephem.next_new_moon(date)
-        pnm = ephem.previous_new_moon(date)
-        lunation = (date - pnm) / (nnm - pnm)
-        return lunation
+    def get_moon_age_fraction(self, date):
+        """ Gets the "age" of the Moon as a fraction of the lunar cycle.
+            New moon = 0.0, full moon = 0.5.
+            Uses the 2 * 4 previous and next "quarter" phases provided by ephem
+            to interpolate between the closest two quarters.
+        """
+        date = ephem.Date(date)
+        prev_dates = [ephem.previous_new_moon(date), ephem.previous_first_quarter_moon(date),
+            ephem.previous_full_moon(date), ephem.previous_last_quarter_moon(date)]
+        max_prev_date = max(prev_dates)
+        max_prev_index = prev_dates.index(max_prev_date)
+        min_next_index = (max_prev_index + 1) % 4
+        min_next_date = [ephem.next_new_moon, ephem.next_first_quarter_moon,
+            ephem.next_full_moon, ephem.next_last_quarter_moon][min_next_index](date)
+        between_quarters_fraction = (date - max_prev_date) / (min_next_date - max_prev_date)
+        moon_age_fraction = 0.25 * (max_prev_index + between_quarters_fraction)
+        return moon_age_fraction
