22f525ffe817ea36037ef490855d53230f80e35e
Peter Kok <kretep@users.noreply.github.com>
2021-09-25 20:12:54 +0200

    Fix buienradar chart line

--- draw/buienradar.py
+++ draw/buienradar.py
@@ -3,5 +3,4 @@ def draw_buienradar_chart(context, x, y, w, h, data):
     context.draw.rectangle((x, y+h, x+w, y))
     n = len(data) - 1
     pts = [(x + int(1.0 * w * i / n), y+h - int(1.0 * h * int(p[1]) / 255)) for i, p in enumerate(data)]
-    context.draw.polygon(pts, outline=context.black, fill=None)
-    context.draw.line([*pts, pts[0]], width=3, fill=context.black)
+    context.draw.line(pts, width=3, fill=context.black)
