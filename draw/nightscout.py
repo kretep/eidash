235796ff08076631d0762e7922354ab49056fa5b
Peter Kok <kretep@users.noreply.github.com>
2022-10-08 20:30:04 +0200

    New nightscout layout

--- draw/nightscout.py
+++ draw/nightscout.py
@@ -1,16 +1,23 @@
 
 def draw_nightscout(context, x, y, w, h, data):
+    minutes_ago = data["minutes_ago"]
+    minutes_ago_text = str(minutes_ago) if minutes_ago >= 0 else "?"
     main_text = f'{data["sgv"]}{data["direction"]}'
-    delta_text = f'{data["delta"]}\n[{data["minutes_ago"]}m]'
+    delta_text = f'{data["delta"]}\n[{minutes_ago_text}m]'
 
     # Determine text size
     main_size = context.draw.textsize(main_text, context.font_normal)
     delta_size = context.draw.textsize(delta_text, context.font_small)
-    x_main = x + (w - main_size[0] - delta_size[0]) / 2
-    x_delta = x_main + main_size[0]
+    main_x = x + (w - main_size[0] - delta_size[0]) / 2
+    delta_x = main_x + main_size[0]
 
     # Main value
-    context.draw.text((x_main, y), main_text, font=context.font_normal, fill=context.black)
+    context.draw.text((main_x, y), main_text, font=context.font_normal, fill=context.black)
+
+    # Strikethrough for old data
+    if minutes_ago > 8:
+        line_y = y + main_size[1] / 2 + 2
+        context.draw.line([(main_x, line_y), (main_x + main_size[0], line_y)], width=3, fill=context.black)
 
     # Delta
-    context.draw.text((x_delta, y), delta_text, font=context.font_small, fill=context.black)
+    context.draw.text((delta_x, y), delta_text, font=context.font_small, fill=context.black)
--- draw/nsdraw.py
+++ draw/nsdraw.py
@@ -8,12 +8,35 @@ class NSDraw:
 
     def __init__(self, width, height):
         self.context = DrawContext(width, height)
-        self.context.font_normal = ImageFont.truetype(os.environ['NSDASH_FONT'], size=54)
+        self.context.font_normal = ImageFont.truetype(os.environ['NSDASH_FONT'], size=72)
         self.context.font_small = ImageFont.truetype(os.environ['NSDASH_FONT'], size=27)
-        self.context.font_time = ImageFont.truetype(os.environ['NSDASH_FONT'], size=18)
+        self.context.font_time = ImageFont.truetype(os.environ['NSDASH_FONT'], size=27)
 
     def draw_data(self, data):
-        self.context.clear_image()
-        draw_time(self.context, self.context.width - 4, 4)
-        y = (self.context.height - self.context.font_normal.size) / 2
-        draw_nightscout(self.context, 0, y, self.context.width, 100, data)
+        context = self.context
+        context.clear_image()
+
+        minutes_ago = data["minutes_ago"]
+        minutes_ago_text = str(minutes_ago) if minutes_ago >= 0 else "?"
+        time_text = datetime.now().strftime("%H:%M")
+        delta_text = f'{data["delta"]}|{minutes_ago_text}m {time_text}'
+        main_text = f'{data["sgv"]}{data["direction"]}'
+
+        # Determine text size and layout
+        delta_size = context.draw.textsize(delta_text, context.font_small)
+        main_size = context.draw.textsize(main_text, context.font_normal)
+        w = context.width
+        delta_x = (w - delta_size[0]) / 2
+        delta_y = 4 # margin
+        main_x = (w - main_size[0]) / 2
+        main_y = delta_y + delta_size[1]
+
+        # Draw
+        context.draw.text((delta_x, delta_y), delta_text, font=context.font_small, fill=context.black)
+        context.draw.text((main_x, main_y), main_text, font=context.font_normal, fill=context.black)
+
+        # Strikethrough for old data
+        if minutes_ago > 8:
+            y = main_y + main_size[1] / 2 + 4
+            context.draw.line([(main_x, y), (main_x + main_size[0], y)], 
+                width=8, fill=context.black)
