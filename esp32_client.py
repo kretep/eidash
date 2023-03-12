d28f6aba6dfd048f552d1e76975f7147cd771ab3
Peter Kok <kretep@users.noreply.github.com>
2023-03-12 11:34:08 +0100

    hkdash client for ESP32

---
+++ esp32_client.py
@@ -0,0 +1,75 @@
+import requests
+import os
+import numpy as np
+import logging
+
+# This function is entirely courtesy of our ChatGPT overlords
+# (it can probably be done more efficient, but it works)
+def encode_bit_array(bits):
+    # Define the mapping from 4-bit strings to characters
+    char_map = {
+        '0000': 'a',
+        '0001': 'b',
+        '0010': 'c',
+        '0011': 'd',
+        '0100': 'e',
+        '0101': 'f',
+        '0110': 'g',
+        '0111': 'h',
+        '1000': 'i',
+        '1001': 'j',
+        '1010': 'k',
+        '1011': 'l',
+        '1100': 'm',
+        '1101': 'n',
+        '1110': 'o',
+        '1111': 'p'
+    }
+
+    # Convert the numpy array of bits to a binary string
+    bin_str = ''.join(['1' if bit else '0' for bit in bits])
+
+    # Break the binary string into groups of four characters each
+    groups = [bin_str[i:i+4] for i in range(0, len(bin_str), 4)]
+
+    # Map each group of four characters to the corresponding character
+    result = ''.join([char_map[group] for group in groups])
+
+    # Swap the position of every two characters
+    result = ''.join([result[i:i+2][::-1] for i in range(0, len(result), 2)])
+
+    return result
+
+def make_request(path):
+    # The "server" running on the ESP32 will not respond with a proper status code,
+    # but will terminate the connection on every call, even when the command is
+    # successful. We do want to impose and catch a timeout however.
+    try:
+        requests.get(path, timeout=2)
+        return True
+    except requests.exceptions.Timeout as e:
+        logging.error("ESP32 request timed out")
+        return False  # Server not available
+    except requests.exceptions.RequestException as e:
+        return True   # We expect this
+
+def send_image(img):
+    host = os.environ['HKDASH_ESP32_HOST']
+    # The server on the ESP32 expects commands and data in the path of the request
+    
+    # Begin write
+    success = make_request(f"{host}/EPDw_")
+    if not success:
+        return  # Don't bother to continue
+    
+    # Write 1000 characters per request
+    bits = np.asarray(img).reshape(-1)
+    for i in range(0, bits.shape[0], 4000):
+        # Data is sent in chunks of 1000 characters, representing 4000 bits
+        line = encode_bit_array(bits[i:(i+4000)])
+        success = make_request(f"{host}/{line}iodaLOAD_")
+        if not success:
+            return  # Don't bother to continue
+
+    # Finalize
+    make_request(f"{host}/SHOW_")
---
+++ hkdash_esp32.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+import time
+from datetime import datetime
+import locale
+import logging
+
+from data.nightscout import NightscoutData
+from data.weather import WeatherData
+from data.ephem import EphemData
+from data.birthdays import BirthdayData
+from data.sunspot_image import SunspotImage
+from data.sunspot_number import SunspotNumber
+from data.buienradar_text import BuienradarText
+
+from draw.hkdraw import HKDraw
+from esp32_client import send_image
+# from pixel.nightscout_pixel import draw_ns_pixel, turn_off_pixel
+
+from dotenv import load_dotenv
+load_dotenv()
+
+logging.basicConfig(level=logging.WARNING)
+
+try:
+    locale.setlocale(locale.LC_TIME, "nl_NL.utf8")
+
+    logging.info("Starting")
+    width = 800
+    height = 480
+
+    logging.info("Init")
+
+    # Initialize data sources
+    dataSources = {
+        "nightscout": NightscoutData(),
+        "weather": WeatherData(),
+        "ephem": EphemData(),
+        "birthdays": BirthdayData(),
+        "sunspot_image": SunspotImage(),
+        "sunspot_number": SunspotNumber(),
+        "buienradar_text": BuienradarText()
+    }
+
+    # Initialize drawing target
+    hkdraw = HKDraw(width, height)
+
+    isStandby = False
+
+    while True:
+        # Check if we should sleep
+        now = datetime.now()
+        if now.hour <= 6:
+            if not isStandby:
+                hkdraw.clear_image()
+                send_image(hkdraw.context.image)
+                #turn_off_pixel()
+                isStandby = True
+        else:
+            isStandby = False
+
+        if not isStandby:
+            # Retrieve data
+            data = {}
+            for key, source in dataSources.items():
+                logging.info("GET DATA for " + key)
+                try:
+                    data[key] = source.get_data()
+                except Exception as err:
+                    data[key] = { "error": str(err) }
+
+            # Save the data (for debugging)
+            # with open('debug_data.json', 'w') as outfile:
+            #     json.dump(data, outfile)
+
+            # Draw
+            hkdraw.draw_data(data)
+
+            # Pixel
+            # if not "error" in data["nightscout"]:
+            #     draw_ns_pixel(data["nightscout"])
+
+            # Display
+            send_image(hkdraw.context.image)
+
+        # Sleep until the next update
+        now = datetime.now()
+        minutes_to_go = 4 - now.minute % 5
+        seconds_to_go = 60 - now.second + minutes_to_go * 60
+        logging.info(f"Sleeping for {seconds_to_go} seconds")
+        time.sleep(seconds_to_go)
+
+except IOError as e:
+    logging.info(e)
+
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    exit()
